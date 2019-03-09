3e1ce13895a57928a069e0bb5e2a9651419ac96c
PenguinLemma <thepenguinlemma@gmail.com>
2019-03-09 20:38:54 +0100

    Add solution to part 2 of day one that doesn't require twisted use of STL algo
    Add note on original solution as to why that is not best practice

--- day1_not_quite_lisp/part2.cpp
+++ day1_not_quite_lisp/part2.cpp
@@ -7,12 +7,23 @@ int main()
 {
     int position = 0;
     int floor = 0;
+    // This can be considered twisted use of `find_if`, given that
+    // elements are not considered individually in terms of the
+    // the condition (since the return of the lambda depends on
+    // a cummulative condition and not an isolated condition)
     std::find_if(
         std::istream_iterator<char>(std::cin),
         std::istream_iterator<char>(),
         [&position, &floor](char c){
             ++position;
-            floor = (c == '(') ? floor + 1 : floor - 1;
+            if (c == '(')
+            {
+                ++floor;
+            }
+            else
+            {
+                --floor;
+            }
             return floor == -1;
         }
     );
---
+++ day1_not_quite_lisp/part2_2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+// Solution that won't make a twisted use of STL algorithms
+int main()
+{
+    int position = 1;
+    int floor = 0;
+    char c;
+    while (std::cin >> c)
+    {
+        if (c == '(')
+        {
+            ++floor;
+        }
+        else
+        {
+            --floor;
+        }
+        if (floor == -1)
+            break;
+
+        ++position;
+    }
+    std::cout << position << std::endl;
+}
